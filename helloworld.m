297e86ee12e8bed8df241660c56d22cccf6913d1
Brian Lam <brianlam@advancedgeos.com>
2021-03-03 14:32:38 -0500

    another commit

--- helloworld.m
+++ helloworld.m
@@ -1,1 +1,19 @@
 %This is a test script
+clc
+clear 
+close all
+
+
+x = 3;
+y = 3;
+[total,subtotal] = addition(x,y);
+
+function [summer,subber] = addition(x,y)
+summer = x+y;
+subber = x-y;
+end
+
+
+
+
+
