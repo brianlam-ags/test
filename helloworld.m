b6758b6742f3a6a1f002563cbd7b33e6d5ec10e6
Brian Lam <brianlam@advancedgeos.com>
2021-03-04 16:39:05 -0500

    bpsk modulation performed using communications toolbox

--- helloworld.m
+++ helloworld.m
@@ -1,19 +1,25 @@
-%This is a test script
+%This script performs BPSK modulation on a data stream
+%using the communications toolbox as opposed to 
+%performing the modulation from scratch
 clc
 clear 
 close all
 
+n = 5000; % However many numbers you want.
+nbits = int32(randi([0, 1], [1, n]));
+trans = transpose(nbits);
 
-x = 3;
-y = 3;
-[total,subtotal] = addition(x,y);
+% A = 1;
+% Fc = 40; % Carrier frequency
+% Fs = 200; % Sampling frequency
+% carrier = A*exp(j*2*pi*(Fc/Fs)*n); %Carrier wave
+% real_carrier = real(carrier);
 
-function [summer,subber] = addition(x,y)
-summer = x+y;
-subber = x-y;
-end
+bpskModulator = comm.BPSKModulator;
+modData = bpskModulator(trans);
 
-
-
+% bpsk = carrier.*IQ; %offset bpsk
+% real_bpsk = real(bpsk);
+plot(real(modData));
 
 
